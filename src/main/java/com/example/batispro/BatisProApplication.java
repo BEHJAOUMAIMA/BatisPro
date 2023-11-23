c592c2a7811ac5fc14d896a7d241b7f3a88e2416
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-11-23 09:29:02 +0100

    init project

---
+++ src/main/java/com/example/batispro/BatisProApplication.java
@@ -0,0 +1,13 @@
+package com.example.batispro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatisProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatisProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/batispro/BatisProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batispro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatisProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
